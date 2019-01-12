50921f44262af06d14a05fce492ef3170b9128e9
Thibaut Maddelein <thibaut.maddelein.y9677@student.hogent.be>
2019-01-12 19:51:33 +0100

    added Podfiles

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '12.1'
+
+target 'school-books' do
+  use_frameworks!
+	
+  pod 'RealmSwift'
+  pod 'Siesta/UI'
+end
+
---
+++ Pods/Target Support Files/Pods-school-books/Pods-school-books-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_school_books : NSObject
+@end
+@implementation PodsDummy_Pods_school_books
+@end
